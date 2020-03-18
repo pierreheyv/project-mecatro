6c30824981e3f20225f2c6ddd7770d70b774c14a
pierreheyv <56588950+pierreheyv@users.noreply.github.com>
2020-03-18 13:21:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,9 +13,12 @@
 #include "struct.h"
 #include "positionctrl.h"
 #include "ia.h"
+#include "speedctrl.h"
 
 
 #define CAN_BR 125e3
+
+//Pour analyser ce code commencer ici à la fonction main et suivre avec fichier structure code
 
 //plus propre si on mettait ça dans un fichier à part avec toutes les fonctions "supplémentaires"
 void writeDataToFile (double data)  // by example "DataPath" = "C:\\program.txt"
@@ -50,27 +53,31 @@ int main()
     can = new CAN(CAN_BR);
     can->configure();
 
-    can->ctrl_motor(1);
-
     SPI_DE0 *spi;
     spi = new SPI_DE0(0,500000);
 
     CtrlStruct* structure = init();
 
+    can->ctrl_motor(1);
     can->push_TowDC(0);
-
     can->push_PropDC(0, 0);
+
+    //path and map initialization (see pathplanning algorithm.c to understand how it works
+    /*
+    structure->theUserStruct->mymap = createmap();
+    structure->theUserStruct->mypath = initpath(mymap);
+    */
 
     while(1)
     {
-        updateinfo(structure, spi);  //update info from laser tower and from encoder (cfr posctrl)
-        //speed, avancement, dist from beam
+        updateinfo(structure, spi);//(cfr posctrl) update info from laser tower and from encoder, speed, avancement, dist from beam
 
-        computecmd(structure);//compute cmd to give to the motors
+        computecmd(structure);//(cfr ia) update the wanted commands
 
-        can->push_PropDC(structure->theCtrlOut->wheel_commands[0], structure->theCtrlOut->wheel_commands[1]);
+        //action : create sub function if need
+        run_speed_controller(structure, can);//(cfr speedctrl )apply the speed (low level controller)
 
+        //other : create sub function if need
         writeDataToFile (structure->theUserStruct->Error_omega_l);
     }
-    can->push_PropDC(0, 0);
 }
