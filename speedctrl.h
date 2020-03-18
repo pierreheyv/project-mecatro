de5e277dc95ff33b579997b79490c66b8b3efcb9
pierreheyv <56588950+pierreheyv@users.noreply.github.com>
2020-03-18 13:22:00 +0100

    Update speedctrl.h

--- speedctrl.h
+++ speedctrl.h
@@ -1,6 +1,9 @@
 #ifndef SPEEDCTRL_H_INCLUDED
 #define SPEEDCTRL_H_INCLUDED
 
+#include "IO/COM/CAN/CAN.hh"
+#include "IO/COM/SPI/Specific/SPI_CAN.hh"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -11,12 +14,12 @@
 #define GAINW 0.011
 #define KP 0.0299
 #define KI 0.2071
-#define SAT 22.8
-#define CURRENTSAT 4//0.74
-#define RA 7.1
+#define SAT 22.8//to be adapted !!!
+#define CURRENTSAT 0.74//to be adapted !!!
+#define RA 7.1//to be adapted !!!
 
 double saturation(double boundaries, double input);
 double CtrlOutCalc(double u_ref);
-void run_speed_controller(CtrlStruct *theCtrlStruct);
+void run_speed_controller(CtrlStruct *theCtrlStruct, CAN *can);
 
 #endif // SPEEDCTRL_H_INCLUDED
