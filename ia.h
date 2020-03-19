20b478edcab601d2422c12845cccd2c2aaba2c16
pierreheyv <56588950+pierreheyv@users.noreply.github.com>
2020-03-19 10:09:53 +0100

    Update ia.h

--- ia.h
+++ ia.h
@@ -4,13 +4,14 @@
 #include "struct.h"
 #include "positionctrl.h"
 #include "speedctrl.h"
+#include "pathplanning.h"
 
 //middle level controller cst
 #define KR 0.5 //coef de rotation
 #define KL 1 //coef linear
 #define WIDTH 0.5 //width between two wheels or odometer (to think)
 
-void computecmd(CtrlStruct *theCtrlStruct);
+void computecmd(CtrlStruct *theCtrlStruct, Map *mymap);
 void takenext_dest(CtrlStruct *theCtrlStruct, double dest[]);//take the next destination in a txt file
 void pathplanning(CtrlStruct *theCtrlStruct); //write list of position to get to the objective
 
