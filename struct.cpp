8b0d57f4fddbab3f013b5b87952a1fb018172d0a
pierreheyv <56588950+pierreheyv@users.noreply.github.com>
2020-03-19 10:10:09 +0100

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -1,6 +1,5 @@
 
 #include "struct.h"
-#include <stdlib.h>
 
 
 
@@ -9,14 +8,11 @@ CtrlStruct* init(){
 	structure->theUserStruct = (UserStruct *) malloc(sizeof(UserStruct));
 	structure->theCtrlIn = (CtrlIn *) malloc(sizeof(CtrlIn));
 	structure->theCtrlOut = (CtrlOut *) malloc(sizeof(CtrlOut));
-	structure->theUserStruct->wantedspeedl = 0;
-	structure->theUserStruct->wantedspeedr = 0;
 	structure->theUserStruct->windup = 1;
 	structure->theUserStruct->cmdtype = 0;
-    structure->theUserStruct->wanteddist = 0;
-	structure->theUserStruct->wantedangle = 0;
 	structure->theUserStruct->period = 0.003;
 	structure->theUserStruct->stime = 0;
+	structure->theUserStruct->calibstate = 0;
 	return structure;
 }
 
