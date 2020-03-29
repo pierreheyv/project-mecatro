58980d2559bb0eba4757e583032ec528f575f8b9
pierreheyv <56588950+pierreheyv@users.noreply.github.com>
2020-03-29 17:11:54 +0200

    Update positionctrl.h

--- positionctrl.h
+++ positionctrl.h
@@ -7,11 +7,13 @@
 #include "speedctrl.h"
 
 #define K 0.005//à modif !!!
+#define DISTTOAVOIDR 0.5//à définir !!!
 
 //info
 void updateinfo(CtrlStruct *theCtrlStruct, SPI_DE0 *spi );
 void computeinfopos(CtrlStruct *theCtrlStruct, SPI_DE0 *spi);
 void motorspeed(CtrlStruct *theCtrlStruct, SPI_DE0 *spi);
 void computedisttobeam(CtrlStruct *theCtrlStruct, SPI_DE0 *spi);
-int adjusttbit (int beforeTreatment); //to treat the information comming from the FPGA (avoid the first dirty bit)
+int adjusttbit (int beforeTreatment); //to treat the information comming from the FPGA (avoid the first dirty byte)
+void needtoavoid(CtrlStruct *theCtrlStruct);
 #endif // POSITIONCTRL_H_INCLUDED
